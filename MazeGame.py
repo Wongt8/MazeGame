243288a67ea894c9271f1292466f46a964cc13ff
maleyy <63105226+Wongt8@users.noreply.github.com>
2022-02-06 19:16:37 +0100

    Breadth algo  (not a good idea but it is working)

--- MazeGame.py
+++ MazeGame.py
@@ -2,60 +2,50 @@ from Maze import *
 from Robot import *
 
 from tkinter import *
-import math
+import queue
 
-
-global maze
 
 level = 1
-width = 20
-height = 30
+width = 10
+height = 10
 
 
-################################################################################################
 
-def poseDecor(maze:Maze):
+def poseDecor(maze:Maze,good_moves:list):
     tab = maze.get_maze()
-    start = maze.get_starting_point()
-    exit = maze.get_exit_point()
     for i in range(len(tab)):
         for j in range(len(tab[i])):
             if tab[i][j] == CellState.WALL:
                 grille.create_rectangle(j*25,i*25,(j+1)*25,(i+1)*25,fill="red")
-            # FOR THE PATH FINDIND
-            # if tab[i][j] == CellState.EMPTY:
-            #     pts = distance(exit,(i,j)) - distance(start,(i,j))
-            #     grille.create_text(j*25+12,i*25+12,text=pts,fill="blue")
+            if (i,j) in good_moves:
+                grille.create_rectangle(j*25,i*25,(j+1)*25,(i+1)*25,fill="blue")
             elif tab[i][j] == CellState.EXIT:
                 grille.create_rectangle(j*25,i*25,(j+1)*25,(i+1)*25,fill="orange")
+            
+
+    
                 
-def avance(direction,maze,fenetre):
+def avance(direction,maze,fenetre,good_moves:list):
     grille.delete(ALL)
-    poseDecor(maze)
+    poseDecor(maze,good_moves)
     pers.avancer(direction,maze.get_maze(),fenetre)
     pers.dessiner(direction,grille)
 
 
 def touche(event):
-    t=event.keysym
-    if t == 'Right' or t == 'd':
+    key = event.keysym
+    if key == 'Right' or key == 'd':
         direction = "E"
-        avance(direction,maze,fenetre)
-    elif t == 'Left' or t == 'q': 
+        avance(direction,maze,fenetre,good_moves)
+    elif key == 'Left' or key == 'q': 
         direction = "W"
-        avance(direction,maze,fenetre)
-    elif t == 'Up' or t == 'z': 
+        avance(direction,maze,fenetre,good_moves)
+    elif key == 'Up' or key == 'z': 
         direction = "N"
-        avance(direction,maze,fenetre)
-    elif t == 'Down' or t == 's': 
+        avance(direction,maze,fenetre,good_moves)
+    elif key == 'Down' or key == 's': 
         direction = "S"
-        avance(direction,maze,fenetre)
-
-
-def distance(p0, p1):
-    # P0 = exit point
-    return round(math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2),2)
-
+        avance(direction,maze,fenetre,good_moves)
 
 def get_oppisite(direction):
     if direction == "N":
@@ -68,40 +58,85 @@ def get_oppisite(direction):
         return "E"
 
 
-def create_value(maze):
+def generate_coord_by_input(maze:Maze,input:list):
+    start = maze.get_starting_point()
+    coord = [start]
+    for move in input:
+        if move == "q":
+            coord.append((coord[-1][0],coord[-1][1]-1))
+        if move == "d":
+            coord.append((coord[-1][0],coord[-1][1]+1))
+        if move == "s":
+            coord.append((coord[-1][0]+1,coord[-1][1]))
+        if move == "z":
+            coord.append((coord[-1][0]-1,coord[-1][1]))
+    return coord
+
+
+def valid(maze:Maze,moves:list):
     maze_ = maze.get_maze()
-    value = [distance(maze.get_exit_point(),(i,j)) - distance(maze.get_starting_point(),(i,j)) if maze_[i][j] == CellState.EMPTY else None for i in range(len(maze_)) for j in range(len(maze_[i]))]
-    return value
+    col,row = maze.get_starting_point()
+    for move in moves:
+        if move == "q": row -= 1
+        elif move == "d": row += 1
+        elif move == "s": col += 1
+        elif move == "z": col -= 1
+        
+        if not(0 <= row < len(maze_[0]) and 0 <= col < len(maze_)):
+            return False
+        elif maze_[col][row] == CellState.WALL:
+            return False
+    return True
 
-def pathfinding(maze:Maze, value:list):
-    #TODO
-    pass
 
 
+def find_end(maze:Maze,moves:str):
+    maze_ = maze.get_maze()
+    col,row = maze.get_starting_point()
+    for move in moves:
+        if move == "q": row -= 1
+        elif move == "d": row += 1
+        elif move == "s": col += 1
+        elif move == "z": col -= 1 
+    if maze_[col][row] == CellState.EXIT:
+        return True
+    return False
 
+def path_finding(maze:Maze):
+    nums = queue.Queue()
+    nums.put("")
+    add = ""
+
+    while not find_end(maze,add):
+        add = nums.get()
+        for move in ["q", "d", "s", "z"]:
+            put = add + move
+            if valid(maze,put):
+                nums.put(put)
+    return generate_coord_by_input(maze,add)
 
 
 
 """ Programme principal"""
-
-# while True:
-fenetre=Tk()
-fenetre.title(f'Labyrinthe Level {level}')
-maze = Maze(width,height,level)
-maze.generate_maze()
-maze.afficher_maze()
-grille=Canvas(fenetre,width=len(maze.get_maze()[0]*25),height=len(maze.get_maze())*25)
-grille.pack()
-poseDecor(maze) # on construit le labyrinthe
-# pathfinding(maze)
-
-pers=Robot(maze.get_starting_point()[1],maze.get_starting_point()[0],"E",grille,maze.get_maze(),fenetre) # on construit le robot
-pers.dessiner("E",grille)
-fenetre.bind_all('<Key>',touche)
-fenetre.mainloop()
-    # level += 1
-    # width += 1
-    # height += 1
+if __name__ == '__main__':
+    global maze,good_moves
+    # while True:
+    fenetre=Tk()
+    fenetre.title(f'Labyrinthe Level {level}')
+    maze = Maze(width,height,level)
+    maze.generate_maze()
+    maze.afficher_maze()
+    grille=Canvas(fenetre,width=len(maze.get_maze()[0]*25),height=len(maze.get_maze())*25)
+    grille.pack()
+    good_moves = list(path_finding(maze))
+    poseDecor(maze,good_moves) # on construit le labyrinthe
+    pers=Robot(maze.get_starting_point()[1],maze.get_starting_point()[0],"E",grille,maze.get_maze(),fenetre) # on construit le robot
+    pers.dessiner("E",grille)
+    fenetre.bind_all('<Key>',touche)
+    fenetre.mainloop()
+        # level += 1
+        # width += 1
+        # height += 1
 
     
 
