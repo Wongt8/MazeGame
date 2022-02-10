aa7771d9ea24cbc9427d1ebb5eb7d18b76652a56
maleyy <63105226+Wongt8@users.noreply.github.com>
2022-02-10 18:17:54 +0100

    Little refactor

--- MazeGame.py
+++ MazeGame.py
@@ -1,13 +1,16 @@
 from Maze import *
 from Robot import *
+from Pathfind import *
+
 
 from tkinter import *
-import queue
 
-
+"""
+Define maze size and level
+"""
+width = 8
+height = 8
 level = 1
-width = 10
-height = 10
 
 
 
@@ -18,10 +21,9 @@ def poseDecor(maze:Maze,good_moves:list):
             if tab[i][j] == CellState.WALL:
                 grille.create_rectangle(j*25,i*25,(j+1)*25,(i+1)*25,fill="red")
             if (i,j) in good_moves:
-                grille.create_rectangle(j*25,i*25,(j+1)*25,(i+1)*25,fill="blue")
+                grille.create_rectangle(j*25,i*25,(j+1)*25,(i+1)*25,fill="grey")
             elif tab[i][j] == CellState.EXIT:
                 grille.create_rectangle(j*25,i*25,(j+1)*25,(i+1)*25,fill="orange")
-            
 
     
                 
@@ -56,65 +58,6 @@ def get_oppisite(direction):
         return "W"
     elif direction == "W":
         return "E"
-
-
-def generate_coord_by_input(maze:Maze,input:list):
-    start = maze.get_starting_point()
-    coord = [start]
-    for move in input:
-        if move == "q":
-            coord.append((coord[-1][0],coord[-1][1]-1))
-        if move == "d":
-            coord.append((coord[-1][0],coord[-1][1]+1))
-        if move == "s":
-            coord.append((coord[-1][0]+1,coord[-1][1]))
-        if move == "z":
-            coord.append((coord[-1][0]-1,coord[-1][1]))
-    return coord
-
-
-def valid(maze:Maze,moves:list):
-    maze_ = maze.get_maze()
-    col,row = maze.get_starting_point()
-    for move in moves:
-        if move == "q": row -= 1
-        elif move == "d": row += 1
-        elif move == "s": col += 1
-        elif move == "z": col -= 1
-        
-        if not(0 <= row < len(maze_[0]) and 0 <= col < len(maze_)):
-            return False
-        elif maze_[col][row] == CellState.WALL:
-            return False
-    return True
-
-
-
-def find_end(maze:Maze,moves:str):
-    maze_ = maze.get_maze()
-    col,row = maze.get_starting_point()
-    for move in moves:
-        if move == "q": row -= 1
-        elif move == "d": row += 1
-        elif move == "s": col += 1
-        elif move == "z": col -= 1 
-    if maze_[col][row] == CellState.EXIT:
-        return True
-    return False
-
-def path_finding(maze:Maze):
-    nums = queue.Queue()
-    nums.put("")
-    add = ""
-
-    while not find_end(maze,add):
-        add = nums.get()
-        for move in ["q", "d", "s", "z"]:
-            put = add + move
-            if valid(maze,put):
-                nums.put(put)
-    return generate_coord_by_input(maze,add)
-
 
 
 """ Programme principal"""
